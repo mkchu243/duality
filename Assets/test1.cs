38f3882a60df5f1f3519cc3a0ea513d361e630fc
mkchu243 <mkchu243@gmail.com>
2013-08-05 17:38:02 -0700

    spin feature

--- Assets/test1.cs
+++ Assets/test1.cs
@@ -5,11 +5,12 @@ public class test1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	//asdfjasldfjlsdfjldsf
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	Vector3 rotationVelocity = new Vector3(45, 90, 1);
+    transform.Rotate(rotationVelocity * Time.deltaTime);
 	}
 }
